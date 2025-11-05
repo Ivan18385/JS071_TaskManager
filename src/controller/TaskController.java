c61b348b413eea3e8d541db6ba31c769a0175d83
Ivan <nguyenan18385@gmail.com>
2025-11-06 00:04:55 +0700

    complete controller

--- src/controller/TaskController.java
+++ src/controller/TaskController.java
@@ -2,9 +2,7 @@ package controller;
 
 import constants.TaskType;
 import model.Task;
-import util.Validation;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
@@ -12,9 +10,35 @@ import java.util.List;
 
 public class TaskController {
     private final List<Task> taskList = new ArrayList<>();
+    private int lastId = 1;
 
 
+    //add task function
+    public void addTask(String requirementName, TaskType taskType, Date date,
+                        double planFrom, double planTo, String assignee, String reviewer) {
+        int newId = lastId++;
+        Task newTask = new Task(newId, requirementName, taskType, date, planFrom, planTo, assignee, reviewer);
+        taskList.add(newTask);
+    }
 
+    //delete task
+    public void deleteTask(int id) {
+        boolean removedTask = taskList.removeIf(task -> task.getId() == id);
+        if (!removedTask) {
+            throw new IllegalArgumentException("Task ID " + id + " not found");
+        }
+    }
+
+    //return a copy of all task
+    public List<Task> getTaskList() {
+        return new ArrayList<>(taskList);
+    }
+
+    public List<Task> displayTaskAscendingId(){
+        List<Task> sortTask = getTaskList();
+        sortTask.sort(Comparator.comparingInt(Task :: getId));
+        return sortTask;
+    }
 
 
 }
